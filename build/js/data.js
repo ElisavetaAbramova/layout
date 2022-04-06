c3edcd54218e32447000da0ea0ac4cf3545b6b55
ElisavetaAbramova <95209047+ElisavetaAbramova@users.noreply.github.com>
2022-04-06 16:30:47 +0300

    Add files via upload

---
+++ build/js/data.js
@@ -0,0 +1,23 @@
+const data = {
+    "first_name": "Elizaveta",
+    "last_name": "Abramova",
+    "job_title": "JUNIOR FRONTEND DEVELOPER",
+    "photo": "images/w1.jpg",
+    "phone": "+79266292035",
+    "email": "elizaveta.abramova@mail.com",
+    "address": "Россия, Москва.",
+    "skills": [
+        "HTML 5",
+        "CSS 3",
+        "Bootstrap",
+        "Git",
+        "Gulp",
+    ],
+    "education": [
+        "Medical College №5",
+    ],
+    "achievements": [
+        "I completed the course in a week and I really liked it.",
+    ],
+    "profile": "I am a front-end engineer developer, I approach my tasks carefully and with enthusiasm. I am looking for an entry-level position to continue to develop in development, use my skills in solving complex problems and help in the timely completion of company projects."
+};
---
+++ src/js/experience.js
@@ -0,0 +1,18 @@
+const experience = [
+    {
+        "company": "Hospital at the Presidente",
+        "city": "MOSCOW",
+        "from": "2017",
+        "to": 'Present',
+        "position": "Head nurse",
+        "description": "<li>Organization of staff work</li> <li>Holding conferences</li> <li>Execution of medical procedures</li>"
+    },
+    {
+        "company": "The Russian Academy of Sciences",
+        "city": "MOSCOW",
+        "from": "2014",
+        "to": '2017',
+        "position": "Nurse",
+        "description": "<li>Execution of medical procedures</li>"
+    }
+]
